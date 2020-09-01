21f8e411d57ee8b85d00662f23d026d2bcf0339c
algoquant <algoquant@algoquants.ch>
2020-08-31 21:03:53 -0400

    Update etf_data.R

--- R/etf_data.R
+++ R/etf_data.R
@@ -4,11 +4,11 @@
 #'
 #' @title The etf_data dataset contains a single environment called etf_env,
 #'   which includes daily \code{OHLC} time series data for a portfolio of
-#'   symbols.
+#'   symbols.  All the prices are already adjusted.
 #'
 #' @description
-#' The etf_env environment includes daily \code{OHLC} time series
-#' data for a portfolio of symbols, and reference data:
+#' The etf_env environment includes daily \code{OHLC} time series data for a
+#' portfolio of symbols, and reference data:
 #' \describe{
 #'   \item{sym_bols}{a \code{vector} of \code{strings} with the portfolio symbols.}
 #'   \item{price_s}{a single \code{xts} time series containing daily closing
@@ -19,21 +19,22 @@
 #'   \code{OHLC} prices for the \code{sym_bols}.}
 #' }
 #'
-#' @format Each \code{xts} time series contains the columns:
+#' @format Each \code{xts} time series contains the following columns with
+#'   adjusted prices and trading volume:
 #' \describe{
 #'   \item{Open}{Open prices}
 #'   \item{High}{High prices}
 #'   \item{Low}{Low prices}
 #'   \item{Close}{Close prices}
 #'   \item{Volume}{daily trading volume}
-#'   \item{Adjusted}{Adjusted closing prices}
 #' }
 #'
 #' @usage data(etf_data)  # not required - data is lazy load
 #'
 #' @examples
-#' # data(etf_data)  # not needed - data is lazy load
-#' # get first six rows of OHLC prices
+#' # Loading is not not needed - data is lazy load
+#' # data(etf_data)
+#' # Get first six rows of OHLC prices
 #' head(etf_env$VTI)
 #' \donttest{chart_Series(x=etf_env$VTI["2009-11"])}
 "etf_env"
